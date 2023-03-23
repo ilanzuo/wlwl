d860afd8b88dc1c82ce3e8483fe4c1e9aaee67d4
lanzuo <lanzuo@lanzuodeMacBook-Pro.local>
2023-03-23 12:37:23 +0800

    init project

---
+++ pages/create/create.vue
@@ -0,0 +1,49 @@
+<template>
+	<view class="content">
+		<view class="form">
+			<view>
+				<text class="legend">数量</text>
+				<input class="ipt" type="text"/>
+			</view>
+			<view>
+				<text class="legend">照片</text>
+				<view class="upload-handler">
+					<image src="@/static/icon-camera.png"></image>
+				</view>
+			</view>
+			<view>
+				<view class="btn-geo">
+					<text>{{geo_district}}</text>
+					<image src="@/static/icon-right.png"></image>
+				</view>
+			</view>
+			<view class="btn-submit"><button>提交</button></view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				geo_district : "枫桥丽舍"
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {height:100%;background-color: $uni-bg-color;position: absolute;width: 100%;}
+	.form {padding:20rpx}
+	.form .legend {display:flex;margin:40rpx 0;font-size: 20rpx;font-weight: bold;}
+	.form .ipt {width:80%;height: 120rpx;background-color:#fff ; text-align: center;}
+	.upload-handler image {width:100rpx;height:100rpx}
+	.btn-geo {margin-top:40rpx;padding-left:25rpx;height:30rpx;line-height:30rpx;font-weight:bold;color:$uni-text-color-grey;background:#F1F1F1 url("@/static/icon-location.png") 0 6rpx no-repeat;background-size: 14rpx 20rpx;}
+	.btn-geo image {margin-left:10rpx;width:10rpx;height:17rpx;position:relative;top:4rpx}
+	.btn-submit {position: fixed;bottom:10px;width:100%}
+	.btn-submit button {padding:0}
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,161 @@
+<template>
+	<view class="content">
+		<view class="header">
+			<image src="@/static/banner.jpeg" mode="aspectFill"></image>
+		</view>
+		
+		<view class="feeds">
+			<view class="title">
+				<text class="tt">已发布</text>
+				<text class="dd">{{number_feed_box}}个盒子，{{number_feed_bag}}个冰袋</text>
+				<navigator url="/pages/create/create">+发布信息</navigator>
+			</view>
+			<block v-for="feed in feeds">
+				<view class="feed">
+					<image :src="feed.img" mode="aspectFill"></image>
+					<view>
+						<text class="status">{{feed.status}}</text>
+						<text class="time">发布时间：{{feed.time}}</text>
+					</view>
+					<view class="score" v-if="feed.score !== 0">+{{feed.score}}</view>
+					<view class="score" v-else><text class="txt-gray">待评估</text></view>
+					
+				</view>
+			</block>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				number_feed_box : 128,
+				number_feed_bag : 88,
+				feeds : [
+					{
+						status : "待回收",
+						time : "2021/12/21 18:23:21",
+						score : 0,
+						img : "/static/box.png"
+					},
+					{
+						status : "已回收",
+						time : "2021/11/21 18:23:21",
+						score : 11,
+						img : "/static/box.png"
+					},
+					{
+						status : "已回收",
+						time : "2021/10/21 15:22:11",
+						score : 11,
+						img : "/static/box.png"
+					},
+					{
+						status : "已回收",
+						time : "2021/11/21 18:23:21",
+						score : 11,
+						img : "/static/box.png"
+					},
+					{
+						status : "已回收",
+						time : "2021/10/21 15:22:11",
+						score : 11,
+						img : "/static/box.png"
+					},
+					{
+						status : "已回收",
+						time : "2021/11/21 18:23:21",
+						score : 11,
+						img : "/static/box.png"
+					},
+					{
+						status : "已回收",
+						time : "2021/10/21 15:22:11",
+						score : 11,
+						img : "/static/box.png"
+					},
+					{
+						status : "已回收",
+						time : "2021/11/21 18:23:21",
+						score : 11,
+						img : "/static/box.png"
+					},
+					{
+						status : "已回收",
+						time : "2021/10/21 15:22:11",
+						score : 11,
+						img : "/static/box.png"
+					},
+					{
+						status : "已回收",
+						time : "2021/11/21 18:23:21",
+						score : 11,
+						img : "/static/box.png"
+					},
+					{
+						status : "已回收",
+						time : "2021/10/21 15:22:11",
+						score : 11,
+						img : "/static/box.png"
+					}
+				]
+			}
+		},
+		onLoad() {
+			this.init_feeds();
+		},
+		methods: {
+			init_feeds : function(){
+				console.log(1111)
+				const { Client } = require("@notionhq/client");
+				// Initializing a client
+				const notion = new Client({
+				  auth: "secret_BSWF2WlFz2YVcFrr2B9IyacELPconZ6K5rweTh3wdWO",
+				});
+				
+				(async () => {
+				  const databaseId = 'c68fe24a8c034e0ca09561ccf9dae727';
+				  const response = await notion.databases.query({
+				    database_id: databaseId
+				  });
+				  console.log(response);
+				  console.log(222);
+				  
+				})();
+			}
+		}
+	}
+	
+	
+</script>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		background-color: $uni-bg-color;
+	}
+
+	.header {
+		display:flex;
+		width:100%;
+		justify-content: center;
+		border-bottom: 1px solid $uni-border-color;
+	}
+	.header image {width:100%}
+	
+	.feeds {margin-top:-45rpx;position: relative;width:100%}
+	.title {padding:0 20rpx 20rpx;font-size: 20rpx;height:28rpx;line-height:28rpx}
+	.title .tt {float: left;font-size:28rpx;padding-right:15rpx}
+	.title navigator { float: right;}
+	
+	.feed {padding:20rpx 20rpx 0 20rpx;display: flex;position:relative}
+	.feed image {margin-right:20rpx;width:80rpx;height:80rpx}
+	.feed .status {display:block;margin:10rpx 0;font-weight:bold;}
+	.feed .score {position: absolute;width:80rpx;height:80rpx;line-height:80rpx;text-align:center;right:0}
+	.feed .txt-gray {color: $uni-text-color-grey;}
+</style>
